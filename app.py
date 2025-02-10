960734d91ed3049c7043eb635b29a5db9ad1018b
Chandana <chowdarychandana607@gmail.com>
2025-02-10 11:35:37 +0530

    c

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 app=Flask(__name__)
-@app.route("/")
+@app.route("/",methods=["GET","POST"])
 def home():
-    return render_template("index.html")
+    
+    if request.method=="POST":
+        msg=request.form.get("message")
+        print(msg)
+    else:
+        return  render_template("index.html",methods=["GET","POST"])
 
-if __name__=="__main__":
-    app.run()
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=5153)
